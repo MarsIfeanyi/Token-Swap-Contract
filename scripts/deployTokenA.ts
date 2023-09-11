f21ea7687026a7e432fe551f03c7dd7a50d312d0
MarsIfeanyi <marschalice@gmail.com>
2023-09-11 03:49:21 +0100

    Upddate and Verify contract

--- scripts/deployTokenA.ts
+++ scripts/deployTokenA.ts
@@ -9,16 +9,17 @@ async function main() {
   // await tokenAContract.waitForDeployment();
   console.log(`tokenAContract deployed to: ${tokenAContract.target}`); // 0x8646Ae354bAfd642d1269b132685f8e05bb3401e
 
-  // if (
-  //   network.config.chainId === 11155111 ||
-  //   (5 && process.env.ETHERSCAN_API_KEY)
-  // ) {
-  //   console.log("Waiting for block confirmations...");
+  if (
+    network.config.chainId === 11155111 ||
+    (5 && process.env.ETHERSCAN_API_KEY)
+  ) {
+    console.log("Waiting for block confirmations...");
 
-  //   //wait for 6 block confirmations before verifying the transaction
-  //   await tokenAContract.waitForDeployment();
-  //   await verify(tokenAContract.target, []);
-  // }
+    //wait for 10 block confirmations before verifying the transaction
+    // @ts-ignore
+    await tokenAContract.waitForDeployment(10);
+    await verify(tokenAContract.target, []);
+  }
 }
 
 main().catch((error) => {
