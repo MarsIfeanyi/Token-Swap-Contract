09af7587a9c6e2bab9c4ffdd0a0804bd72485a33
MarsIfeanyi <marschalice@gmail.com>
2023-09-12 12:45:34 +0100

    Create folder and file for contractAddresses

---
+++ scripts/contractAddress/address.ts
@@ -0,0 +1,7 @@
+export const TokenSwapAddress = "0x06Cd06001b0DcDe582a2e9cef8E13D35CE45f674";
+
+export const TokenA_Address = "0x8646ae354bafd642d1269b132685f8e05bb3401e";
+
+export const TokenB_Address = "0xB6e41410b3A58062177547684073e19E8d5b5B74";
+
+export const ownerAddress = "0xC76F962e24F4345301296Bf111529047ec3cA96E";
