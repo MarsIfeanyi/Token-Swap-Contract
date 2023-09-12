14a28e096b777da162829486e6aa2889e3b7f32d
MarsIfeanyi <marschalice@gmail.com>
2023-09-12 12:46:46 +0100

    Create deploy script for Contract

---
+++ scripts/deployContract/TokenSwap.ts
@@ -0,0 +1,35 @@
+import { ethers, run, network } from "hardhat";
+const { verify } = require("../../utils/verify");
+import { TokenA_Address, TokenB_Address } from "../contractAddress/address";
+
+async function main() {
+  const tokensAddresses = [TokenA_Address, TokenB_Address];
+
+  const tokenSwapContract = await ethers.deployContract(
+    "TokenSwap",
+    tokensAddresses
+  );
+
+  console.log("\n===========Deploying TokenSwapContract================");
+  // @ts-ignore
+  await tokenSwapContract.waitForDeployment(10);
+
+  console.log(`tokenSwapContract deployed to: ${tokenSwapContract.target}`);
+
+  // CONTRACT VERIFICATIONS
+
+  if (
+    network.config.chainId === 11155111 ||
+    (5 && process.env.ETHERSCAN_API_KEY)
+  ) {
+    console.log("Waiting for block confirmations...");
+    // @ts-ignore
+    await tokenSwapContract.waitForDeployment(10);
+    await verify(tokenSwapContract.target, [TokenA_Address, TokenB_Address]);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
